771bda6c39cdfeceb57eb2b31d40a24ca68a774d
Leonardo <leo.pelonero@hotmail.it>
2024-12-06 11:50:49 +0100

    setup di un progetto e import di obj in coordinate fisse

--- part_2/js/cesiumConfig.js
+++ part_2/js/cesiumConfig.js
@@ -1,11 +1,15 @@
-const cesiumAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4ZDk0OTM5My01ZjI0LTRhNzctYWVkMi03ODQ2MjljMzgxOWUiLCJpZCI6MjU5NzA2LCJpYXQiOjE3MzMyMjY1NzR9.s8TT0O17qitRVQmhAlCaCqjwP9AwLOGi8ddYuxpThag';
+const cesiumAccessToken = 'token'
 
 const targetLocation = {
-    destination: Cesium.Cartesian3.fromDegrees(-122.4175, 37.655, 400),
+    destination: Cesium.Cartesian3.fromDegrees(9.04409459281873, 48.833844931564137, 20),
     orientation: {
         heading: Cesium.Math.toRadians(0.0),
         pitch: Cesium.Math.toRadians(-15.0),
     }
 };
 
-export { cesiumAccessToken, targetLocation };
+const url = {
+    'treeGlb': './glbData/tree.glb'
+}
+
+export { cesiumAccessToken, targetLocation, url };
--- part_2/js/index.js
+++ part_2/js/index.js
@@ -1,4 +1,6 @@
-import { cesiumAccessToken, targetLocation } from "./cesiumConfig.js";
+import { cesiumAccessToken, targetLocation, url } from "./cesiumConfig.js";
+import { trees } from "./coordinate.js";
+import { createModel } from "./cesiumFun.js";
 
 // Your access token can be found at: https://ion.cesium.com/tokens.
 // Replace `your_access_token` with your Cesium ion access token.
@@ -7,6 +9,12 @@ Cesium.Ion.defaultAccessToken = cesiumAccessToken;
 
 // Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
 const viewer = new Cesium.Viewer('cesiumContainer');
+
+// Creiamo i modelli (tree) mancanti
+// createModel(viewer, url, lon, lat, height)
+trees.features.forEach(feature => {
+    createModel(viewer, url.treeGlb, feature.geometry.coordinates[0], feature.geometry.coordinates[1], 0)
+});
 
 // Fly the camera to San Francisco at the given longitude, latitude, and height.
 viewer.camera.flyTo(targetLocation);
